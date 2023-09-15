cc85b2c01bdc0f7b9f9cc9ee03a7b0707175e190
Watson <watson1978@gmail.com>
2023-09-16 07:50:18 +0900

    Add Statement#bind

--- ext/ilios/statement.c
+++ ext/ilios/statement.c
@@ -3,6 +3,62 @@
 static void statement_destroy(void *ptr);
 static size_t statement_memsize(const void *ptr);
 
+static int hash_cb(VALUE key, VALUE value, VALUE statement)
+{
+    CassandraStatement *cassandra_statement = (CassandraStatement *)statement;
+    const char *name;
+
+    if (SYMBOL_P(key)) {
+        key = rb_sym2str(key);
+    }
+    name = StringValueCStr(key);
+
+    switch (rb_type(value)) {
+    case T_NIL:
+        cass_statement_bind_null_by_name(cassandra_statement->statement, name);
+        break;
+
+    case T_TRUE:
+        cass_statement_bind_bool_by_name(cassandra_statement->statement, name, cass_true);
+
+    case T_FALSE:
+        cass_statement_bind_bool_by_name(cassandra_statement->statement, name, cass_false);
+
+    case T_FIXNUM:
+        cass_statement_bind_int64_by_name(cassandra_statement->statement, name, NUM2LONG(value));
+        break;
+
+    case T_FLOAT:
+        cass_statement_bind_double_by_name(cassandra_statement->statement, name, NUM2DBL(value));
+        break;
+
+    case T_STRING:
+        cass_statement_bind_string_by_name(cassandra_statement->statement, name, StringValueCStr(value));
+        break;
+
+    case T_SYMBOL:
+        value = rb_sym2str(value);
+        cass_statement_bind_string_by_name(cassandra_statement->statement, name, StringValueCStr(value));
+        break;
+
+    default:
+        rb_raise(rb_eTypeError, "Unsupported %"PRIsVALUE" type: %s=%"PRIsVALUE"", rb_obj_class(value), name, value);
+    }
+
+    return ST_CONTINUE;
+}
+
+static VALUE statement_bind(VALUE self, VALUE hash)
+{
+    CassandraStatement *cassandra_statement;
+
+    Check_Type(hash, T_HASH);
+    TypedData_Get_Struct(self, CassandraStatement, &cassandra_statement_data_type, cassandra_statement);
+
+    rb_hash_foreach(hash, hash_cb, (VALUE)cassandra_statement);
+    return self;
+}
+
 const rb_data_type_t cassandra_statement_data_type = {
     "Ilios::Cassandra::Statement",
     {
@@ -39,4 +95,6 @@ static size_t statement_memsize(const void *ptr)
 void Init_statement(void)
 {
     rb_undef_alloc_func(cStatement);
+
+    rb_define_method(cStatement, "bind", statement_bind, 1);
 }

a367bb0b4f74f8b775fd1952ce8c7a5f3c3ea75f
Watson <watson1978@gmail.com>
2024-02-04 07:49:54 +0900

    Rubocop: Fix Style/GuardClause

--- test/helper.rb
+++ test/helper.rb
@@ -65,12 +65,12 @@ end
 def verify_gc_compaction
   # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
   # move objects around, helping to find object movement bugs.
-  if defined?(GC.verify_compaction_references) == 'method'
-    if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.2.0')
-      GC.verify_compaction_references(expand_heap: true, toward: :empty)
-    else
-      GC.verify_compaction_references(double_heap: true, toward: :empty)
-    end
+  return unless defined?(GC.verify_compaction_references) == 'method'
+
+  if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.2.0')
+    GC.verify_compaction_references(expand_heap: true, toward: :empty)
+  else
+    GC.verify_compaction_references(double_heap: true, toward: :empty)
   end
 end
 

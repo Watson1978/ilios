56e205c01c7e5c69d905e9f9801539d0a3f1446c
Shizuo Fujita <fujita@clear-code.com>
2025-04-29 15:12:15 +0900

    Bump version to v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 1.0.2
+
+- Fix install error with CMake 4.x
+
 ## 1.0.1
 
 - Update libuv version to 1.50.0 (#18)
--- lib/ilios/version.rb
+++ lib/ilios/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Ilios
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
   public_constant :VERSION
 
   CASSANDRA_CPP_DRIVER_VERSION = '2.17.1'

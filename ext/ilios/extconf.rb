3efe08783d0897eb99faf7d0becbad6c11fea91e
Shizuo Fujita <fujita@clear-code.com>
2025-06-10 08:10:53 +0900

    Revert "Fix build error with -c++11-extensions"
    This reverts commit 1eb8cd3886a1f45287fd167b8b9d9bf6d2929e14.

--- ext/ilios/extconf.rb
+++ ext/ilios/extconf.rb
@@ -77,10 +77,7 @@ module CassandraDriverInstaller
       flags = super
       flags << "-DCMAKE_POLICY_VERSION_MINIMUM='3.5'"
       # Avoid "Unsupported compiler: AppleClang" on macOS
-      if RUBY_PLATFORM.include?('darwin') && RbConfig::CONFIG['CC'].include?('clang')
-        flags << "-DCMAKE_CXX_FLAGS='-Wno-c++11-extensions'"
-        flags << '-DCMAKE_CXX_COMPILER_ID=Clang'
-      end
+      flags << '-DCMAKE_CXX_COMPILER_ID=Clang' if RUBY_PLATFORM.include?('darwin') && RbConfig::CONFIG['CC'].include?('clang')
 
       flags
     end
